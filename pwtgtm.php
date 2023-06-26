ae5ea9519168c21705703f92b1f09abbf6feeca5
Hans Kuijpers <hans2103@gmail.com>
2023-06-26 13:58:46 +0200

    add consent storage

--- pwtgtm.php
+++ pwtgtm.php
@@ -67,6 +67,26 @@ class PlgSystemPwtgtm extends CMSPlugin
 			return;
 		}
 
+		$consentScript = "
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		
+		if(localStorage.getItem('consentMode') === null){
+			gtag('consent', 'default', {
+				'ad_storage': 'denied',
+				'analytics_storage': 'denied',
+				'personalization_storage': 'denied',
+				'functionality_storage': 'denied',
+				'security_storage': 'denied',
+			});
+		} else {
+			gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')));
+		}
+		";
+
+		$document->getWebAssetManager()
+			->addInlineScript($consentScript);
+
 		// Google Tag Manager - party loaded in head
 		$headScript = "
 		<!-- Google Tag Manager -->
